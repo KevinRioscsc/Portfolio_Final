53bcdfa353d59e6aecd33adb3f5ad8e09f946d17
Kevin Rios <rioskevin89@gmail.com>
2023-01-10 15:17:42 -0800

    cut react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import RoutesLinks from "./RoutesLinks";
-import { BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home";
 import IsLoaded from "./Context/isLoaded";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <IsLoaded>
-      <BrowserRouter>
-        <RoutesLinks />
-      </BrowserRouter>
+      <Home />
     </IsLoaded>
   </React.StrictMode>
 );
